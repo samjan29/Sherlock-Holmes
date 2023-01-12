f2982fa2998d17246e82fbc5c0a40718dc84fe48
Ryum <ryumforart@gmail.com>
2023-01-12 15:19:10 +0900

    Initial : repository clear to merge dev

--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    const testArray = ['a', 'b', 'c'];
-
-    for (let i = 1; i <= testArray.length ; i++) {
-        $('.quizList > ul').append(`<a href = "{{ url_for('quiz') }}"><li></li></a>`);
-    }; 
-});
--- static/js/quiz.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    let score = 0.23;
-                        
-    $('.wrong_rate').append(`오답률 : ${score}%`);
-});
